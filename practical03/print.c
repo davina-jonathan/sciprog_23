e464e14f9b8345fdfa69abab2c865d9c855ea2f5
davina-jonathan <davinasarah.3@gmail.com>
2023-11-28 16:26:06 +0000

    add practical03

---
+++ practical03/print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int i1 = 10;
+int i2 = 200;
+float fl1 = 1.11;
+float fl2 = 2.2222;
+printf("Two ints %d %d and two floats %.2f %.4f", i1, i2, fl1, fl2);
+return 0;
+}
+
